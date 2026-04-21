c4b2ce245a8ecba8f4637f8495f9af4a33ebea13
LaJub33 <jubin.te@gmail.com>
2026-04-21 09:37:13 +0200

    new scroll detector js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 ScrollReveal().reveal('.reveal');
+
+
+
+const sections = document.querySelectorAll('#Solution, #Offre, #Propos, #Contact');
+const navLinks = document.querySelectorAll('.nav-links a');
+
+
+
+window.addEventListener('scroll', () => {
+    let current = '';
+
+    sections.forEach(section => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top <= 150) {
+            current = section.id;
+        }
+    });
+
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href') === `#${current}`) {
+            link.classList.add('active');
+        }
+    });
+});
